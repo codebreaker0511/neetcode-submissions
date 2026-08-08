d9c6cc83794a501126b6639c8de42b9fbc33f82c
Prashant Kumar <entrepreneurprashant51@gmail.com>
2026-08-09 02:19:48 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int ans = nums[0];
+        int cur = nums[0];
+        for(int i = 1; i < n; i++){
+            cur = max(cur + nums[i], nums[i]);
+            ans = max(cur, ans);
+        }
+        return ans; 
+    }
+};
