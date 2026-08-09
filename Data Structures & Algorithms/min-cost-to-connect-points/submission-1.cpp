f07c9d45951d8b21fa29db15c9fb8c2e5312167f
Prashant Kumar <entrepreneurprashant51@gmail.com>
2026-08-09 12:41:36 +0530

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    //minimum spannning trees??
+    //kruskal or prim?? 
+    struct DSU{
+        vector<int> parent, sz; 
+        DSU(int n){
+            parent.assign(n,-1);
+            sz.assign(n,1);
+            for(int i = 0; i < n; i++){
+                parent[i] = i; 
+            }
+        }
+        int find(int v){
+            if(parent[v] == v) return v; 
+            return parent[v] = find(parent[v]);
+        }
+
+        void unite(int a, int b){
+            a = find(a);
+            b = find(b);
+            if(a != b){
+                if(sz[a] < sz[b]) swap(a,b);
+                sz[a] += sz[b];
+                parent[b] = a; 
+            }
+        }
+    };
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<tuple<int,int,int>> edges;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int x1 = points[i][0]; int x2 = points[j][0];
+                int y1 = points[i][1]; int y2 = points[j][1];
+                int dis = abs(x1-x2) + abs(y1-y2);
+                edges.push_back({dis,i,j});
+            }
+        }
+        sort(edges.begin(), edges.end());
+        DSU dsu(n);
+        int ans = 0; 
+        int edg = 0; 
+        for(auto [wt,u,v]: edges){
+            if(dsu.find(u) != dsu.find(v)){
+                dsu.unite(u,v);
+                ans += wt; 
+                edg++; 
+                if(edg == n-1) break;
+            }
+        }
+        return ans; 
+    }
+};
